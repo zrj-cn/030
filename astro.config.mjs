91514ca5892afe659110b1c10620c3e224b07deb
Wonderzerika <134346616+Wonderzerika@users.noreply.github.com>
2025-04-01 16:36:13 +0800

    add solution

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,7 +23,7 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://zrj030.vercel.app/",
   base: "/",
   trailingSlash: "always",
 
